8c775cc79ae213d49f0619c002538d7238a30ea8
Michael Cruz <mrcruz1989@gmail.com>
2023-11-20 16:12:31 -0500

    a8 a9

--- rust-course-data/exercises/activities/src/bin/a7.rs
+++ rust-course-data/exercises/activities/src/bin/a7.rs
@@ -10,4 +10,32 @@
 // * Use a match expression to determine which color
 //   name to print
 
-fn main() {}
+enum Color {
+    Red,
+    Blue,
+    Green,
+    RgbColor(u8, u8, u8), // tuple
+    CmykColor{cyan: u8, magenta: u8, yellow: u8, black: u8}, // struct
+}
+
+fn print_color(c: Color) {
+    match c {
+        Color::Red => println!("red"),
+        Color::Blue => println!("blue"),
+        Color::Green => println!("green"),
+        Color::RgbColor(0, 0, 0) => println!("black"),
+        Color::RgbColor(r, g, b) => println!("rgb({}, {}, {})", r, g, b),
+        Color::CmykColor{cyan: _, magenta: _, yellow: _, black: 255} => println!("black"),
+        Color::CmykColor{cyan: c, magenta: m, yellow: y, black: k} => println!("cmyk({}, {}, {}, {})", c, m, y, k),
+    }
+}
+
+fn main() {
+    print_color(Color::Red);
+    print_color(Color::Blue);
+    print_color(Color::Green);
+    print_color(Color::RgbColor(0, 0, 0));
+    print_color(Color::RgbColor(255, 255, 255));
+    print_color(Color::CmykColor{cyan: 0, magenta: 0, yellow: 0, black: 255});
+    print_color(Color::CmykColor{cyan: 100, magenta: 100, yellow: 100, black: 100});
+}
--- rust-course-data/exercises/activities/src/bin/a8.rs
+++ rust-course-data/exercises/activities/src/bin/a8.rs
@@ -5,8 +5,32 @@
 //
 // Notes:
 // * Use an enum to create different flavors of drinks
+
 // * Use a struct to store drink flavor and fluid ounce information
+enum Flavor {
+    Orange,
+    Grape,
+    LemonLime,
+}
+struct Drink {
+    flavor: Flavor,
+    ounces: f64,
+}
 // * Use a function to print out the drink flavor and ounces
 // * Use a match expression to print the drink flavor
+fn print_drink(drink: Drink) {
+    match drink.flavor {
+        Flavor::Orange => println!("orange"),
+        Flavor::Grape => println!("grape"),
+        Flavor::LemonLime => println!("lemon-lime"),
+    }
+    println!("{} ounces", drink.ounces);
+}
 
-fn main() {}
+fn main() {
+    let drink = Drink {
+        flavor: Flavor::Orange,
+        ounces: 12.0,
+    };
+    print_drink(drink);
+}
