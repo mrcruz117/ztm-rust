828cdca0865cb5950439cdba684eeee59979473f
Michael Cruz <mrcruz1989@gmail.com>
2023-11-20 14:50:58 -0500

    up to a5

--- rust-course-data/exercises/activities/src/bin/a4b.rs
+++ rust-course-data/exercises/activities/src/bin/a4b.rs
@@ -10,4 +10,18 @@
 // * Use a match expression to determine which message to display
 // * Use an underscore (_) to match on any value
 
-fn main() {}
+fn display_number(num: i32) {
+    match num {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        _ => println!("other"),
+    }
+}
+
+fn main() {
+    display_number(1);
+    display_number(2);
+    display_number(3);
+    display_number(4);
+}
--- rust-course-data/exercises/activities/src/bin/a5.rs
+++ rust-course-data/exercises/activities/src/bin/a5.rs
@@ -9,4 +9,23 @@
 // * Print the variable within the loop statement
 // * Use break to exit the loop
 
-fn main() {}
+use std::time::Instant;
+
+fn count_to_n (n: i32) {
+    let mut i = 1;
+    loop {
+        println!("{}", i);
+        i += 1;
+        if i > n {
+            break;
+        }
+    }
+}
+
+fn main() {
+    let start = Instant::now();
+    count_to_n(15);
+    let duration = start.elapsed();
+
+    println!("Time elapsed in count_to_n is: {:?}", duration);
+}
