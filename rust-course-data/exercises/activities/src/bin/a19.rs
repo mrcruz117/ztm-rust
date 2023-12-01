c5fc633a241bfcd11bc726a92d0aef2e2724b242
mrcruz117 <mrcruz1989@gmail.com>
2023-11-30 20:31:27 -0500

    hashmaps

--- rust-course-data/exercises/activities/src/bin/a19.rs
+++ rust-course-data/exercises/activities/src/bin/a19.rs
@@ -13,4 +13,28 @@
 // Notes:
 // * Use a HashMap for the furniture store stock
 
-fn main() {}
+
+use std::collections::HashMap;
+
+struct InvItem {
+    amount:   i32,
+}
+
+fn main() {
+    let mut inventory = HashMap::new();
+    inventory.insert("Chairs", InvItem { amount: 5 });
+    inventory.insert("Beds", InvItem { amount: 3 });
+    inventory.insert("Tables", InvItem { amount: 2 });
+    inventory.insert("Couches", InvItem { amount: 0 });
+
+    let mut total = 0;
+    for (key, value) in &inventory {
+        if value.amount == 0 {
+            println!("{}: Out of stock", key);
+        } else {
+            println!("{}: {}", key, value.amount);
+        }
+        total += value.amount;
+    }
+    println!("Total: {}", total);
+}
--- rust-course-data/exercises/activities/src/bin/pg.rs
+++ rust-course-data/exercises/activities/src/bin/pg.rs
@@ -1,3 +1,33 @@
 // coding playground for Rust
+use std::collections::HashMap;
 
-fn main() {}
+#[derive(Debug)]
+struct Contents {
+    contents: String,
+}
+
+fn main() {
+    let mut lockers = HashMap::new();
+    lockers.insert(
+        1,
+        Contents {
+            contents: "books".to_string(),
+        },
+    );
+    lockers.insert(
+        2,
+        Contents {
+            contents: "homework".to_string(),
+        },
+    );
+    lockers.insert(
+        3,
+        Contents {
+            contents: "supplies".to_string(),
+        },
+    );
+
+    for (key, value) in &lockers {
+        println!("{:?}: {:?}", key, value.contents);
+    }
+}
