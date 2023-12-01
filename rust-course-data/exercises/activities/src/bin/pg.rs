972fea1faa6c3212065c64f5e382bd5959d99dee
Michael Cruz <mrcruz1989@gmail.com>
2023-12-01 15:23:48 -0500

    option combinators

--- rust-course-data/exercises/activities/src/bin/pg.rs
+++ rust-course-data/exercises/activities/src/bin/pg.rs
@@ -1,33 +1,20 @@
 // coding playground for Rust
-use std::collections::HashMap;
-
-#[derive(Debug)]
-struct Contents {
-    contents: String,
-}
 
 fn main() {
-    let mut lockers = HashMap::new();
-    lockers.insert(
-        1,
-        Contents {
-            contents: "books".to_string(),
-        },
-    );
-    lockers.insert(
-        2,
-        Contents {
-            contents: "homework".to_string(),
-        },
-    );
-    lockers.insert(
-        3,
-        Contents {
-            contents: "supplies".to_string(),
-        },
-    );
+    let a: Option<i32> = Some(1);
+    dbg!(a);
+    let a_is_some = a.is_some();
+    dbg!(a_is_some);
 
-    for (key, value) in &lockers {
-        println!("{:?}: {:?}", key, value.contents);
-    }
+    let a_is_none = a.is_none();
+    dbg!(a_is_none);
+
+    let a_mapped = a.map(|x| x + 1);
+    dbg!(a_mapped);
+    let a_filtered = a.filter(|x| x > &1);
+    dbg!(a_filtered);
+    let a_or_else = a.or_else(|| Some(5));
+    dbg!(a_or_else);
+    let unwrapped = a.unwrap_or_else(|| 0);
+    dbg!(unwrapped);
 }
