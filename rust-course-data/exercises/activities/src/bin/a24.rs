d954b15fc1136f48199dc81a62c342b8a0f8d90f
mrcruz117 <mrcruz1989@gmail.com>
2023-12-02 16:34:09 -0500

    a24 iter activity

--- rust-course-data/exercises/activities/src/bin/a24.rs
+++ rust-course-data/exercises/activities/src/bin/a24.rs
@@ -10,4 +10,14 @@
 
 fn main() {
     let data = vec![1, 2, 3, 4, 5];
+
+    let result: Vec<_> = data
+        .iter()
+        .map(|x| x * 3)
+        .filter(|x| x > &10)
+        .collect();
+
+    for x in result {
+        println!("{}", x);
+    }
 }
