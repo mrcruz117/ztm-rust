d1ad5c07bdac360115b7dad8cb6803344001b5ff
Michael Cruz <mrcruz1989@gmail.com>
2023-12-05 14:57:42 -0500

    external crates demo

--- rust-course-data/exercises/activities/src/bin/pg.rs
+++ rust-course-data/exercises/activities/src/bin/pg.rs
@@ -1,16 +1,10 @@
 // coding playground for Rust
+use humantime::format_duration;
+use std::time::Duration;
+
+
 
 fn main() {
-    let numbers = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-    let mut plus_one_vec = vec![];
-
-    for number in &numbers {
-        plus_one_vec.push(number + 1);
-    }
-
-    let plus_one_iter: Vec<_> = numbers.iter().map(|number| number + 1).collect();
-
-    println!("push {:?}", plus_one_vec);
-    println!("iter {:?}", plus_one_iter);
+    let d = Duration::from_secs(123456789);
+    println!("{}", format_duration(d));
 }
