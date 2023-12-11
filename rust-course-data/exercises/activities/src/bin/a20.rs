9132e2c6469b8bb6965d0e15ca7b9ba2dc33c16a
Michael Cruz <mrcruz1989@gmail.com>
2023-12-11 16:09:35 -0500

    a20 user input activity

--- rust-course-data/exercises/activities/src/bin/a20.rs
+++ rust-course-data/exercises/activities/src/bin/a20.rs
@@ -23,4 +23,51 @@
 // * The program should be case-insensitive (the user should be able to type
 //   Reboot, reboot, REBOOT, etc.)
 
-fn main() {}
+use std::io;
+
+enum PowerState {
+    Off,
+    Sleep,
+    Reboot,
+    Shutdown,
+    Hibernate,
+}
+
+impl PowerState {
+    fn new(state: &str) -> Option<PowerState> {
+        match state.trim().to_lowercase().as_str() {
+            "off" => Some(PowerState::Off),
+            "sleep" => Some(PowerState::Sleep),
+            "reboot" => Some(PowerState::Reboot),
+            "shutdown" => Some(PowerState::Shutdown),
+            "hibernate" => Some(PowerState::Hibernate),
+            _ => None,
+        }
+    }
+}
+
+fn print_power_action(state: PowerState) {
+    use PowerState::*;
+    match state {
+        Off => println!("Powering off"),
+        Sleep => println!("Sleeping"),
+        Reboot => println!("Rebooting"),
+        Shutdown => println!("Shutting down"),
+        Hibernate => println!("Hibernating"),
+    }
+}
+
+fn main() {
+    let mut buffer = String::new();
+    println!("Enter a power state: ");
+    let user_input = io::stdin().read_line(&mut buffer);
+
+    if user_input.is_ok() {
+        match PowerState::new(&buffer) {
+            Some(state) => print_power_action(state),
+            None => println!("Invalid power state"),
+        }
+    } else {
+        println!("Error reading input");
+    }
+}
