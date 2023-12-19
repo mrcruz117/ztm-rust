ca6549b023b553309f883326d28bab4f5baebb1f
Michael Cruz <mrcruz1989@gmail.com>
2023-12-19 16:55:41 -0500

    traits a25

--- rust-course-data/exercises/activities/src/bin/a25.rs
+++ rust-course-data/exercises/activities/src/bin/a25.rs
@@ -12,4 +12,39 @@
 // * Use a single function to print out the perimeter of the shapes
 //   * The function must utilize impl trait as a function parameter
 
-fn main() {}
+trait Perimeter {
+    fn perimeter(&self) -> i32;
+}
+
+struct Square {
+    side: i32,
+}
+
+struct Triangle {
+    a: i32,
+    b: i32,
+    c: i32,
+}
+
+impl Perimeter for Square {
+    fn perimeter(&self) -> i32 {
+        self.side * 4
+    }
+}
+
+impl Perimeter for Triangle {
+    fn perimeter(&self) -> i32 {
+        self.a + self.b + self.c
+    }
+}
+
+fn print_perimeter<T: Perimeter>(shape: T) {
+    println!("Perimeter: {:?}", shape.perimeter());
+}
+
+fn main() {
+    let square = Square { side: 4 };
+    let triangle = Triangle { a: 3, b: 4, c: 5 };
+    print_perimeter(square);
+    print_perimeter(triangle);
+}
